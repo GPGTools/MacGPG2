884d4b69a06f1f0699fa201d2741fe9e912f0904
Benjamin Donnachie <benjamin@py-soft.co.uk>
2011-02-02 15:42:39 +0000

    Change to default build options.  Remains experimental.
    Signed-off-by: Benjamin Donnachie <benjamin@py-soft.co.uk>

--- build-script.sh
+++ build-script.sh
@@ -27,10 +27,13 @@ export MacGPG2="/usr/local/MacGPG2"
 
 export MACOSX_DEPLOYMENT_TARGET=10.5
 export CFLAGS="-mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET -isysroot /Developer/SDKs/MacOSX$MACOSX_DEPLOYMENT_TARGET.sdk -arch i386 -arch x86_64"
+export CXXFLAGS="$CFLAGS"
 export CPPFLAGS=-I$MacGPG2/include
 export LDFLAGS=-L$MacGPG2/lib
 export CC=/usr/bin/gcc-4.0
 export CXX=/usr/bin/g++-4.0
+
+export configureFlags="--enable-static=no --disable-maintainer-mode --disable-dependency-tracking --prefix=$MacGPG2"
 
 # Create a build directory a level above
 #  Really need build directory now self contained in $MacGPG2?
@@ -44,21 +47,25 @@ status Either run script as root with sudo or be available to enter password for
 
 status First pass of libiconv
 cd $WorkingDirectory/source/libiconv
-try ./configure --enable-static=no --disable-dependency-tracking --prefix=$MacGPG2 --enable-extra-encodings
+try ./configure $configureFlags --enable-extra-encodings
 try make
 try sudo make install
 
 status gettext
 cd $WorkingDirectory/source/gettext
-try ./configure --enable-static=no --disable-dependency-tracking --prefix=$MacGPG2
+try ./configure $configureFlags --disable-csharp --disable-native-java --without-emacs \
+ --with-included-gettext --with-included-glib --with-included-libcroco --with-included-libxml \
+  --disable-java 
 try make
 try sudo make install
 try sudo make -e prefix=$BuildDirectory install
+#try sudo rm -fr $MacGPG2/share/doc/gettext/
+#try sudo rm -fr $BuildDirectory/share/doc/gettext/
 
 status libiconv
 cd $WorkingDirectory/source/libiconv
 try make distclean
-try ./configure --enable-static=no --disable-dependency-tracking --prefix=$MacGPG2 --enable-extra-encodings
+try ./configure $configureFlags --enable-extra-encodings
 try make
 try sudo make install
 try sudo make -e prefix=$BuildDirectory install
@@ -66,7 +73,7 @@ try sudo make -e prefix=$BuildDirectory install
 # Next pth
 status pth
 cd $WorkingDirectory/source/pth
-try ./configure --enable-static=no --disable-dependency-tracking --prefix=$MacGPG2 --with-mctx-mth=sjlj --with-mctx-dsp=ssjlj --with-mctx-stk=sas
+try ./configure $configureFlags --with-mctx-mth=sjlj --with-mctx-dsp=ssjlj --with-mctx-stk=sas
 # Patch make for multiple architecture builds
 try patch -p0 < Makefile.patch
 try make
@@ -76,45 +83,45 @@ try sudo make -e prefix=$BuildDirectory install
 
 status libusb
 cd $WorkingDirectory/source/libusb
-try ./configure --enable-static=no --disable-dependency-tracking --prefix=$MacGPG2
+try ./configure $configureFlags
 try make
 try sudo make install
 try sudo make -e prefix=$BuildDirectory install
 
-export LIBUSB_1_0_CFLAGS="-I/usr/local/MacGPG2/include"
-export LIBUSB_1_0_LIBS="-L$MacGPG2/lib -lusb"
+#export LIBUSB_1_0_CFLAGS="-I/usr/local/MacGPG2/include/libusb-1.0/"
+#export LIBUSB_1_0_LIBS="-L$MacGPG2/lib"
 
 status lib-compat
 cd $WorkingDirectory/source/libusb-compat
-try ./configure --enable-static=no --disable-dependency-tracking --prefix=$MacGPG2
+try ./configure $configureFlags
 try make
 try sudo make install
 try sudo make -e prefix=$BuildDirectory install
 
 status libgpg-error
 cd $WorkingDirectory/source/libgpg-error
-try ./configure --enable-static=no --disable-dependency-tracking --prefix=$MacGPG2
+try ./configure $configureFlags
 try make
 try sudo make install
 try sudo make -e prefix=$BuildDirectory install
 
 status libassuan
 cd $WorkingDirectory/source/libassuan
-try ./configure --enable-static=no --disable-dependency-tracking --prefix=$MacGPG2 --with-gpg-error-prefix=$MacGPG2/
+try ./configure $configureFlags --with-gpg-error-prefix=$MacGPG2/
 try make
 try sudo make install
 try sudo make -e prefix=$BuildDirectory install
 
 status libgcrypt
 cd $WorkingDirectory/source/libgcrypt
-try ./configure --enable-static=no --disable-dependency-tracking --prefix=$MacGPG2 --with-gpg-error-prefix=$MacGPG2 --with-pth-prefix=$MacGPG2 --disable-asm
+try ./configure $configureFlags --with-gpg-error-prefix=$MacGPG2 --with-pth-prefix=$MacGPG2 --disable-asm --disable-endian-check
 try make
 try sudo make install
 try sudo make -e prefix=$BuildDirectory install
 
 status libksba
 cd $WorkingDirectory/source/libksba
-try ./configure --enable-static=no --disable-dependency-tracking --prefix=$MacGPG2 --with-gpg-error-prefix=$MacGPG2/
+try ./configure $configureFlags --with-gpg-error-prefix=$MacGPG2/
 try make
 try sudo make install
 try sudo make -e prefix=$BuildDirectory install
@@ -122,7 +129,7 @@ try sudo make -e prefix=$BuildDirectory install
 # make clean currently broke in GnuPG2
 status GnuPG2
 cd $WorkingDirectory/source/gnupg2
-try ./configure --disable-dependency-tracking --with-pinentry-pgm=$MacGPG2/libexec/pinentry-mac.app/Contents/MacOS/pinentry-mac --prefix=$MacGPG2 --enable-standard-socket --with-gpg-error-prefix=$MacGPG2/ --with-libgcrypt-prefix=$MacGPG2/ --with-libassuan-prefix=$MacGPG2/ --with-ksba-prefix=$MacGPG2/ --with-pth-prefix=$MacGPG2/ --disable-gpgtar --with-libiconv-prefix=$MacGPG2 --with-libintl-prefix=$MacGPG2/
+try ./configure $configureFlags --with-pinentry-pgm=$MacGPG2/libexec/pinentry-mac.app/Contents/MacOS/pinentry-mac --prefix=$MacGPG2 --enable-standard-socket --with-gpg-error-prefix=$MacGPG2/ --with-libgcrypt-prefix=$MacGPG2/ --with-libassuan-prefix=$MacGPG2/ --with-ksba-prefix=$MacGPG2/ --with-pth-prefix=$MacGPG2/ --disable-gpgtar --with-libiconv-prefix=$MacGPG2 --with-libintl-prefix=$MacGPG2/
 try make
 try sudo make install
 try sudo make -e prefix=$BuildDirectory install
