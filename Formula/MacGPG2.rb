cd2e1c1d18b598bfa742fc2bd71811ecc3be8cad
Lukas Pitschl <lukas@dressyvagabonds.com>
2012-01-12 02:48:58 +0100

    - Add the capability to gnupg2 to look for locale dir in ../share/locale. If no locale dir is found return the one inserted at compile time. Fix translations which were caused by a PACKAGE_NAME modified by homebrew.

--- Formula/MacGPG2.rb
+++ Formula/MacGPG2.rb
@@ -94,21 +94,76 @@ index 5f2e31e..d797b68 100644
  #endif /*!HAVE_W32_SYSTEM*/
  }
  
+diff --git a/common/homedir.c b/common/homedir.c
+index bdce3d1..4cc4ab9 100644
+--- a/common/homedir.c
++++ b/common/homedir.c
+@@ -338,6 +338,37 @@ gnupg_localedir (void)
+     }
+   return name;
+ #else /*!HAVE_W32_SYSTEM*/
++  char path[3096];
++  uint32_t size = sizeof(path);
++  if (_NSGetExecutablePath(path, &size) != 0)
++      printf("buffer too small to get executable path; need size %u\n", size);
++  
++  char actualpath [PATH_MAX];
++  char *ptr;
++  ptr = realpath(path, actualpath);
++  // Find the last / in the path. 
++  char *c = strrchr(ptr, (int)'/');
++  // Set the post to the / in the path.
++  int pos = c - ptr;
++  // Only copy the without the executable name.
++  char *dirname_path = malloc((pos * sizeof(char)) + 1);
++  memcpy(dirname_path, ptr, pos);
++  dirname_path[pos+1] = '\0';
++  /* Check if ../share/locale exists. If so, use that path
++   * otherwise try LOCALEDIR.
++   */
++  char *locale_path = NULL;
++  asprintf(&locale_path, "%s/../share/locale", dirname_path);
++  // The path contains relative parts, so let's make them absolute.
++  char complete_path[PATH_MAX];
++  char *final_dir;
++  final_dir = realpath(locale_path, complete_path);
++  char *real_dir = complete_path == NULL ? final_dir : complete_path;
++  // Test if the locale dir exists.
++  struct stat s;
++  if(stat(real_dir, &s) == 0 && s.st_mode & S_IFDIR)
++      return real_dir;
++  // Not found, return the fixed localedir.
+   return LOCALEDIR;
+ #endif /*!HAVE_W32_SYSTEM*/
+ }
 
-# rename package to avoid conflicts with our gnupg 1.x formula
-diff --git a/configure b/configure
-index 3df3900..b102aec 100755
---- a/configure
-+++ b/configure
-@@ -558,8 +558,8 @@ MFLAGS=
- MAKEFLAGS=
+diff --git a/common/homedir.c b/common/homedir.c
+index 48f1e75..d7898d8 100644
+--- a/common/homedir.c
++++ b/common/homedir.c
+@@ -21,6 +21,11 @@
+ #include <stdlib.h>
+ #include <errno.h>
+ #include <fcntl.h>
++#include <limits.h>
++#include <stdio.h>
++#include <stdint.h>
++#include <string.h>
++#include <sys/stat.h>
  
- # Identity of this package.
--PACKAGE_NAME='gnupg'
--PACKAGE_TARNAME='gnupg'
-+PACKAGE_NAME='gnupg2'
-+PACKAGE_TARNAME='gnupg2'
- PACKAGE_VERSION='2.0.18'
- PACKAGE_STRING='gnupg 2.0.18'
- PACKAGE_BUGREPORT='http://bugs.gnupg.org'
+ #ifdef HAVE_W32_SYSTEM
+ #include <shlobj.h>
 
+diff --git a/common/i18n.c b/common/i18n.c
+index db5ddf5..c34fcc7 100644
+--- a/common/i18n.c
++++ b/common/i18n.c
+@@ -37,7 +37,7 @@ i18n_init (void)
+ #else
+ # ifdef ENABLE_NLS
+   setlocale (LC_ALL, "" );
+-  bindtextdomain (PACKAGE_GT, LOCALEDIR);
++  bindtextdomain (PACKAGE_GT, gnupg_localedir ());
+   textdomain (PACKAGE_GT);
+ # endif
+ #endif
