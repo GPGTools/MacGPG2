8aaec21bece68f150260082020e29f93bd5cdc05
Lukas Pitschl <lukas@dressyvagabonds.com>
2012-03-29 08:52:09 +0200

    Fixes pinentry compile for non-ppc

--- Formula/pinentry.rb
+++ Formula/pinentry.rb
@@ -6,20 +6,31 @@ class Pinentry < Formula
   md5 '2095fbe7cf556062427e04c156503f27'
 
   # depends_on 'cmake' => :build
-
+  
+  def patches
+    { :p0 => DATA }
+  end
+  
   def install
     ENV.universal_binary if ARGV.build_universal?
     ENV.build_ppc if ARGV.build_ppc?
     
     target = "compile"
     build_dir = "Release"
+    xconfig = "homebrew.xconfig"
     if ARGV.build_ppc?
       target = "compile_with_ppc"
       build_dir = "Release with ppc"
-      inreplace 'pinentry-mac.xcodeproj/project.pbxproj' do |s|
-        s.gsub! /SDKROOT\s*=\s*.*;/, "SDKROOT = \"#{ARGV.build_env}/SDKs/MacOSX10.5.sdk\";"
-        s.gsub! /GCC_VERSION\s*=\s*.*;/, "GCC_VERSION = com.apple.compilers.llvmgcc42;"
+      xconfig = "homebrew-ppc.xconfig"
+      build_env = ARGV.build_env.gsub ' ', '\\\ '
+      inreplace 'homebrew-ppc.xconfig' do |s|
+         s.gsub! '#SDKROOT#', "#{build_env}/SDKs/MacOSX10.5.sdk"
       end
+    end
+    
+    # Use the homebrew.xconfig file to force using GGC_VERSION specified.
+    inreplace 'Makefile' do |s|
+      s.gsub! /@xcodebuild/, "@xcodebuild -xcconfig #{xconfig}"
     end
     
     system "make #{target}" # if this fails, try separate make/make install steps
@@ -31,3 +42,22 @@ class Pinentry < Formula
     Pathname.new("#{HOMEBREW_PREFIX}/libexec/pinentry-mac.app").make_relative_symlink("#{prefix}/libexec/pinentry-mac.app")
   end
 end
+
+__END__
+
+diff --git homebrew.xconfig homebrew.xconfig
+new file mode 100644
+index 0000000..fdb4290
+--- /dev/null
++++ homebrew.xconfig
+@@ -0,0 +1 @@
++GCC_VERSION = com.apple.compilers.llvmgcc42
+
+diff --git homebrew-ppc.xconfig homebrew-ppc.xconfig
+new file mode 100644
+index 0000000..9515ac6
+--- /dev/null
++++ homebrew-ppc.xconfig
+@@ -0,0 +1,2 @@
++GCC_VERSION = com.apple.compilers.llvmgcc42
++SDKROOT = #SDKROOT#
