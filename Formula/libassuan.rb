07c6d308006bde49dad8bdc6aa19e8c36d35bbf9
Lukas Pitschl <lukas@dressyvagabonds.com>
2013-01-10 22:06:21 +0100

    Run tests after building libassuan.

--- Formula/libassuan.rb
+++ Formula/libassuan.rb
@@ -27,6 +27,7 @@ class Libassuan < Formula
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}",
            "--with-gpg-error-prefix=#{HOMEBREW_PREFIX}",
            "--enable-static=no", "--disable-maintainer-mode"
+    system "make check"
     system "make install"
   end
 end
