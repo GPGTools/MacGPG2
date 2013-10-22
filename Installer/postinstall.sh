44531f3e4c1765a94598c8cf22b568231545adf5
Mento <Mento@gpgtools.org>
2013-10-22 13:39:44 +0200

    [FIX] gpg-agent first load bug fixed.

--- Installer/postinstall.sh
+++ Installer/postinstall.sh
@@ -114,17 +114,20 @@ function cleanOldGpg {
 }
 
 function loadLaunchAgents {
+	killall -KILL gpg-agent
 	nudo launchctl remove org.gpgtools.macgpg2.gpg-agent
+	nudo launchctl unload /Library/LaunchAgents/org.gpgtools.macgpg2.fix.plist
+	
+	shtdwn_pid=$(ps -xo pid,command | sed -nE 's/^([^ ]+) .*\/shutdown\-gpg-agent.*$/\1/p')
+	if [[ -n "$shtdwn_pid" ]] ;then
+		kill -kill "$shtdwn_pid" # Kill shutdown-gpg-agent.
+		wait "$shtdwn_pid"
+	fi
 	
 	nudo launchctl unload /Library/LaunchAgents/org.gpgtools.macgpg2.shutdown-gpg-agent.plist
-	killall -KILL gpg-agent
+	
 	# Run the fixer once as root, to fix potential permission problems.
 	sudo /usr/local/MacGPG2/libexec/fixGpgHome "$USER" "${GNUPGHOME:-$HOME/.gnupg}"
-	# Load the fixer only after, since we are not sure, when the command runs.
-	# If it is launched before the script is called directly, the two scripts might run at the
-	# same time, which leads to problems with the socket creation.
-	nudo launchctl unload /Library/LaunchAgents/org.gpgtools.macgpg2.fix.plist
-	nudo launchctl load /Library/LaunchAgents/org.gpgtools.macgpg2.fix.plist
 	nudo launchctl load /Library/LaunchAgents/org.gpgtools.macgpg2.shutdown-gpg-agent.plist
 	return 0
 }
