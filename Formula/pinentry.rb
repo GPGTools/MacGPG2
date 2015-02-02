3fe00df81dc0b78deeaaf82f1702d6fdf4075b55
Mento <Mento@gpgtools.org>
2015-02-02 23:59:42 +0100

    Use newest pinentry version.

--- Formula/pinentry.rb
+++ Formula/pinentry.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Pinentry < Formula
   homepage 'http://gpgtools.org'
-  head 'https://github.com/GPGTools/pinentry-mac.git', :revision => '98d883de855ca543cf6989665527e9cabfa6ebd4'
+  head 'https://github.com/GPGTools/pinentry-mac.git', :revision => 'HEAD'
   sha1 ''
   version '0.8.1'
   # depends_on 'cmake' => :build
